b8c0f13a87f7e261034a63c5899b9b884e4b0fc2
Yurii Tsimbal <60786111+sellsteam13@users.noreply.github.com>
2021-01-29 15:04:45 +0200

    Доработки
    + очередь выполнения функций

--- assets/js/methods.core.js
+++ assets/js/methods.core.js
@@ -8,3 +8,39 @@ const method = {
         alert(textMessage);
     }
 }
+
+var Queue = (function() {
+    function Queue() {};
+
+    Queue.prototype.running = false;
+
+    Queue.prototype.queue = [];
+
+    Queue.prototype.add_function = function(callback) {
+        var _this = this;
+        this.queue.push(function() {
+            var finished = callback();
+            if (typeof finished === "undefined" || finished) {
+                _this.next();
+            }
+        });
+
+        if (!this.running) {
+            this.next();
+        }
+
+        return this;
+    }
+
+    Queue.prototype.next = function() {
+        this.running = false;
+        var shift = this.queue.shift();
+        if (shift) {
+            this.running = true;
+            shift();
+        }
+    }
+
+    return Queue;
+
+})();
